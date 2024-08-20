bbe2c3e53eaf6b8629976d44a0f4bf2dc8c83500
castleaaron <castleaaron@icloud.com>
2024-08-20 09:39:08 -0500

    api stuff

--- src/Home.js
+++ src/Home.js
@@ -1,16 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import './assets/css/Home.css';
+import axios from 'axios';
 
 function Home() {
-    
-      return (
-     <div className="Home">
-        <header className="Home-header">
-          <p>
-             HELLO
-          </p>
-        </header>
-     </div>
-      );
-    }
+  const [posts, setPosts] = useState([]);
 
-    export default Home;
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const res = await axios.get('.components/api/Posts');
+        setPosts(res.data);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+
+    fetchPosts();
+  }, []);
+
+  return (
+    <div className="Home">
+      <header className="Home-header">
+        <p>HELLO</p>
+        <div>
+          {posts.map(post => (
+            <div key={post._id}>
+              <h2>{post.title}</h2>
+              <p>{post.content}</p>
+            </div>
+          ))}
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default Home;
